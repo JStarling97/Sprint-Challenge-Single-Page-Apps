690d7ff74f0dfca3682feb831b85ed99e836fa9b
Joshua Starling <joshstarling97@gmail.com>
2019-10-11 23:06:00 -0400

    mvp finished.. did some styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar.js";
 import Header from "./components/Header.js";
-
+import SearchForm from "./components/SearchForm.js";
+import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Navbar />
+      <AppRouter />
+      <SearchForm />
     </main>
   );
 }
